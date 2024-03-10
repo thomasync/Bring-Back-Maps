d5e2e0dff989b96160b4002854a56e5c7fd6efce
thomasync <git@thomasync.dev>
2024-03-10 10:24:50 +0100

    fix: permissions

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,4 +3,4 @@
 version=$(grep '"version":' manifest.json | sed 's/.*"\([0-9.]*\)".*/\1/')
 filename="bringbackmaps-$version.zip"
 
-zip -r build/$filename . -x "scripts/*" -x "build/*" -x ".git/*" -x ".history/*"
+zip -r build/$filename . -x "scripts/*" -x "build/*" -x ".git/*" -x ".history/*" -x ".DS_Store"
