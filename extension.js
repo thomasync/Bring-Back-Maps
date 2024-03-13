7ec699ffbc26e3ff5adf4ff61b6bedff5cdd8bc9
Jakub Jeżewski <jakub@jezewski.net>
2024-03-13 18:18:32 +0100

    feat: legacy ui support

--- extension.js
+++ extension.js
@@ -20,16 +20,27 @@
 		}
 	}
 
-	function addMapsToTabs(tabs, address) {
-		const mapsTab = document.createElement('a');
-		mapsTab.innerHTML = `
-				<div jsname="bVqjv" class="GKS7s">
-					<span class="FMKtTb UqcIvb" jsname="pIvPIe">Maps</span>
-				</div>
-			`;
-		mapsTab.className = 'nPDzT T3FoJb';
-		mapsTab.href = address;
-		tabs.insertBefore(mapsTab, tabs.firstChild);
+	function addMapsToTabs(tabs, address, legacy) {
+		if (!legacy) {
+			const mapsTab = document.createElement('a');
+			mapsTab.innerHTML = `
+					<div jsname="bVqjv" class="GKS7s">
+						<span class="FMKtTb UqcIvb" jsname="pIvPIe">Maps</span>
+					</div>
+				`;
+			mapsTab.className = 'nPDzT T3FoJb';
+			mapsTab.href = address;
+			tabs.insertBefore(mapsTab, tabs.firstChild);
+		} else {
+			const mapsTab = document.createElement('div');
+			mapsTab.innerHTML = `
+					<a class="nPDzT T3FoJb" jsname="VIftV" role="link">
+						<div jsname="bVqjv" class="YmvwI">Maps</div>
+					</a>
+				`;
+			mapsTab.firstElementChild.href = address;
+			tabs.insertBefore(mapsTab, tabs.children[1]);
+		}
 	}
 
 	function formatSearchLink(search) {
@@ -53,23 +64,32 @@
 	}
 
 	function app() {
+		let legacy = false;
+		let tabs = document.querySelector('.IUOThf');
+
+		if (!tabs) {
+			// Some of the Google domains use old UI
+			tabs = document.querySelector('.crJ18e');
+
+			if (!tabs) {
+				return
+			}
+
+			legacy = true;
+		}
+
 		const search = document.querySelector('textarea.gLFyf');
-		const tabs = document.querySelector('.IUOThf');
 		const map = document.querySelector('.Ggdpnf');
 		const address = document.querySelector('.gqkR3b a');
 		const largeMap = document.querySelector('.o8ebK');
 
-		if (!tabs) {
-			return;
-		}
-
 		if (map && address) {
 			addHyperLinkMap(map, address);
-			addMapsToTabs(tabs, address.href);
+			addMapsToTabs(tabs, address.href, legacy);
 			loaded = true;
 		} else if (largeMap && search) {
 			addHyperLinkLargeMap(largeMap, search);
-			addMapsToTabs(tabs, formatSearchLink(search.value));
+			addMapsToTabs(tabs, formatSearchLink(search.value), legacy);
 			loaded = true;
 		}
 	}
